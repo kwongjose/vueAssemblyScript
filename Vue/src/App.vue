359562cf29209171938c7742dceae416a2c89ded
Kwong <joseph.kwong@tsi.corp.com>
2019-11-19 14:43:00 -0600

    base app and loop componets

--- Vue/src/App.vue
+++ Vue/src/App.vue
@@ -1,30 +1,46 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <loopCount 
+    :btnText=nativeBtn
+    @click=nativeLoop
+    :result=nativeResult
+    />
+    <loopCount
+      :btnText=assemBtn
+      :result=assemResult
+      @click=assemLoop
+      />
   </div>
 </template>
 
 <script>
+import LoopCount from "./componets/loopCount.vue"
 export default {
   name: 'app',
+  components: {
+    LoopCount
+  },
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      nativeBtn: "Native",
+      nativeResult: "",
+      assemBtn: "Assembly",
+      assemResult: "",
+
+    }
+  },
+  methods: {
+    nativeLoop() {
+      console.log("native");
+      var start = performance.now();
+      for(let i = 1; i < 100; i++) {
+
+      }
+      var end = performance.now();
+      this.nativeResult = (end - start).toString();
+    },
+    assemLoop() {
+
     }
   }
 }
---
+++ Vue/src/componets/loopCount.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="LoopCount">
+        <input type="button" :value="btnText" @click="handleInput">
+        <input type="text" disabled id="timeSpent" :value="result">
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        btnText: {
+            type: String,
+            required: false
+        },
+        result: {
+            type: String,
+            required: false
+        },
+        calculation: {
+            type: Function,
+            required: false
+        }
+    },
+
+    // data() {
+    //     return { };
+    // },
+
+    methods: {
+        handleInput(event) {
+            console.log("click");
+            this.$emit("click");
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
